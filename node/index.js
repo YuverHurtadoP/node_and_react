0bfc71f70a8539babbf972d76e9e76b5a88e7a6b
YuverHurtadoP <santiagohurtadopea@gmail.com>
2025-05-02 22:06:03 -0500

    se crear servidor con express

--- node/index.js
+++ node/index.js
@@ -1,1 +1,13 @@
-console.log("hola que tal");
+const express = require('express');
+ const app = express();
+
+
+ app.get('/',(req, res)=>{
+    res.send('hello from node js');
+ })
+ app.get('/farewell',(req, res)=>{
+    res.send('bye, thanks you');
+ })
+ app.listen(4000 ,()=>{
+    console.log("run server");
+ })
